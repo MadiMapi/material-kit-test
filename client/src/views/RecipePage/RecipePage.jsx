78f954ed82496809ff348f758df4c6f8e1437c0f
Mapi Pina <mpina09@gmail.com>
2018-07-14 09:13:50 -0400

    removed image from recipe page

--- client/src/views/RecipePage/RecipePage.jsx
+++ client/src/views/RecipePage/RecipePage.jsx
@@ -17,7 +17,7 @@ import RecipeList from "./Sections/RecipeList.jsx";
 import RecipeListItem from "./Sections/RecipeListItem.jsx";
 import Input from "@material-ui/core/Input";
 
-import profile from "../../assets/img/faces/christian.jpg";
+import profile from "../../assets/img/food.jpg";
 
 import profilePageStyle from "../../assets/jss/material-kit-react/views/profilePage.jsx";
 
@@ -118,41 +118,6 @@ class RecipePage extends React.Component {
                   </RecipeList>
                 )}
               </GridContainer>
-
-              {/* <GridContainer justify="center">
-                <GridItem xs={12} sm={12} md={8} className={classes.navWrapper}>
-                  <NavPills alignCenter color="primary" tabs={[{ tabButton: "Studio", tabIcon: Camera, tabContent: <GridContainer justify="center">
-                            <GridItem xs={12} sm={12} md={4}>
-                              <img alt="..." src={studio1} className={navImageClasses} />
-                              <img alt="..." src={studio2} className={navImageClasses} />
-                            </GridItem>
-                            <GridItem xs={12} sm={12} md={4}>
-                              <img alt="..." src={studio5} className={navImageClasses} />
-                              <img alt="..." src={studio4} className={navImageClasses} />
-                            </GridItem>
-                          </GridContainer> }, { tabButton: "Work", tabIcon: Palette, tabContent: <GridContainer justify="center">
-                            <GridItem xs={12} sm={12} md={4}>
-                              <img alt="..." src={work1} className={navImageClasses} />
-                              <img alt="..." src={work2} className={navImageClasses} />
-                              <img alt="..." src={work3} className={navImageClasses} />
-                            </GridItem>
-                            <GridItem xs={12} sm={12} md={4}>
-                              <img alt="..." src={work4} className={navImageClasses} />
-                              <img alt="..." src={work5} className={navImageClasses} />
-                            </GridItem>
-                          </GridContainer> }, { tabButton: "Favorite", tabIcon: Favorite, tabContent: <GridContainer justify="center">
-                            <GridItem xs={12} sm={12} md={4}>
-                              <img alt="..." src={work4} className={navImageClasses} />
-                              <img alt="..." src={studio3} className={navImageClasses} />
-                            </GridItem>
-                            <GridItem xs={12} sm={12} md={4}>
-                              <img alt="..." src={work2} className={navImageClasses} />
-                              <img alt="..." src={work1} className={navImageClasses} />
-                              <img alt="..." src={studio1} className={navImageClasses} />
-                            </GridItem>
-                          </GridContainer> }]} />
-                </GridItem>
-              </GridContainer> */}
             </div>
           </div>
         </div>
